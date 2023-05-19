de9a01aa18600c6e7f5b26e14998e9df8d582ba6
elves trindade <elvestrindade31@gmail.com>
2023-05-19 20:26:20 +0100

    chore: add jest

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+export default {
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.spec.ts'],
+  testEnvironment: 'jest-environment-node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
