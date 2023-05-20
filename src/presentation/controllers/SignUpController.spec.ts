07ee3c8fa4d737b7317ba962ffd953b9f4df727b
elves trindade <elvestrindade31@gmail.com>
2023-05-20 18:09:02 +0100

    test: ensure SignUpController returns 400 if no name is provided

---
+++ src/presentation/controllers/SignUpController.spec.ts
@@ -0,0 +1,16 @@
+import { SignUpController } from './SignUpController'
+
+describe('Signup controller', () => {
+  it('Should return 400 if no name is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        email: 'any_email@gmail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toEqual(400)
+  })
+})
