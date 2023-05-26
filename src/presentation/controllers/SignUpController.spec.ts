d1500ca732b6405e8c6f842e3722e81ad8a433c2
elves trindade <elvestrindade31@gmail.com>
2023-05-26 06:57:49 +0100

    test: ensure SignUpController if password Confirmation fails

--- src/presentation/controllers/SignUpController.spec.ts
+++ src/presentation/controllers/SignUpController.spec.ts
@@ -80,6 +80,21 @@ describe('Signup controller', () => {
     expect(httpResponse.body).toEqual(new MissingParamError('passwordConfirmation'))
   })
 
+  it('Should return 400 if password confirmation fails', () => {
+    const { sut } = mockSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@gmail.com',
+        password: 'any_password',
+        passwordConfirmation: 'invalid_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new InvalidParamError('passwordConfirmation'))
+  })
+
   it('Should return 400 if an invalid email is provided', () => {
     const { sut, emailValidatorStub } = mockSut()
     jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
