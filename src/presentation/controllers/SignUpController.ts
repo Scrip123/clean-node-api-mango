33bbbc2bef9390a8b3451466f527d44217c14b8a
elves trindade <elvestrindade31@gmail.com>
2023-05-26 06:56:11 +0100

    feat: ensure SignUpController if password Confirmation fails

--- src/presentation/controllers/SignUpController.ts
+++ src/presentation/controllers/SignUpController.ts
@@ -13,6 +13,9 @@ export class SignUpController implements IController {
           return badRequest(new MissingParamError(field))
         }
       }
+      if (httpRequest.body.passwordConfirmation !== httpRequest.body.password) {
+        return badRequest(new InvalidParamError('passwordConfirmation'))
+      }
       const isValid = this.emailValidator.isValid(httpRequest.body.email)
       if (!isValid) {
         return badRequest(new InvalidParamError('email'))
