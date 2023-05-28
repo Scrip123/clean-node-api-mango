b22743e0ea23ff5d39fe6b34c94e9bfc8d9d82c5
elves trindade <elvestrindade31@gmail.com>
2023-05-28 09:13:49 +0100

    feat: ensure EmailValidator returns false if validator returns false

---
+++ utils/emailValidatorAdapter.ts
@@ -0,0 +1,7 @@
+import { IEmailValidator } from '@presentation/protocols/IEmailValidator'
+
+export class EmailValidatorAdapter implements IEmailValidator {
+  isValid (email: string): boolean {
+    return false
+  }
+}
