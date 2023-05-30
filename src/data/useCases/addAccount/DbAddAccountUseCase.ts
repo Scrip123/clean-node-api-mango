380d7aa8085ca90a61ce8cd44562d37ba73ee972
elves trindade <elvestrindade31@gmail.com>
2023-05-30 18:33:14 +0100

    feat: ensure DbAddAccountUseCase calls Encrypter  with correct values

---
+++ src/data/useCases/addAccount/DbAddAccountUseCase.ts
@@ -0,0 +1,11 @@
+import { IAccountModelDataBase } from '@domain/models/IAccountModel'
+import { IAddAccount, IAddAccountModel } from '@domain/useCases/IAddAcount'
+import { IEncrypter } from 'data/protocols/IEncrypter'
+
+export class DbAddAcountUseCase implements IAddAccount {
+  constructor (private readonly encrypter: IEncrypter) {}
+  async add (account: IAddAccountModel): Promise<IAccountModelDataBase> {
+    await this.encrypter.encrypt(account.password)
+    return await new Promise((resolve, reject) => { resolve(null) })
+  }
+}
