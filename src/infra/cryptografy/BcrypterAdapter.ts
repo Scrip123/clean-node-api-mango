19482800c2dacb92d5d2de56dc5cf2f814f7a8d4
elves trindade <elvestrindade31@gmail.com>
2023-06-01 18:33:37 +0100

    refactor: apply clean code in encrypt method in BcrypterAdapter class

--- src/infra/cryptografy/BcrypterAdapter.ts
+++ src/infra/cryptografy/BcrypterAdapter.ts
@@ -4,6 +4,7 @@ import { IEncrypter } from 'data/protocols/IEncrypter'
 export class BcrypterAdapter implements IEncrypter {
   constructor (private readonly salt: number) {}
   async encrypt (value: string): Promise<string> {
-    return await bcrypt.hash(value, this.salt)
+    const hash = await bcrypt.hash(value, this.salt)
+    return hash
   }
 }
