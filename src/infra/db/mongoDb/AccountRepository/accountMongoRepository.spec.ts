18540d2beff32df10fdff1b033141169b5a44776
elves trindade <elvestrindade31@gmail.com>
2023-06-04 01:20:22 +0100

    test: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongoDb/AccountRepository/accountMongoRepository.spec.ts
@@ -0,0 +1,23 @@
+import { MongoHelper } from '../helpers/mongoHelper'
+import { AccountMongoRepository } from './AccountMongoRepository'
+describe('Account MongoDb Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+  afterAll(async () => {
+    await MongoHelper.desconnect()
+  })
+  it('Should return an account on success', async () => {
+    const sut = new AccountMongoRepository()
+    const account = await sut.add({
+      name: 'any_name',
+      email: 'any_email@gmail.com',
+      password: 'any_password'
+    })
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toEqual('any_name')
+    expect(account.email).toEqual('any_email@gmail.com')
+    expect(account.password).toEqual('any_password')
+  })
+})
--- src/infra/db/mongoDb/helpers/mongoHelper.ts
+++ src/infra/db/mongoDb/helpers/mongoHelper.ts
@@ -3,7 +3,9 @@ import { Collection, MongoClient } from 'mongodb'
 export const MongoHelper = {
   client: null as MongoClient,
   async connect (url: string): Promise<void> {
-    this.client = await MongoClient.connect(process.env.MONGO_URL)
+    this.client = await MongoClient.connect(process.env.MONGO_URL, {
+      useUnifiedTopology: true
+    })
   },
   async desconnect (): Promise<void> {
     await this.client.close()
