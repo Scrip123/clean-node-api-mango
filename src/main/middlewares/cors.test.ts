20c85a7529a6e0cda51d87528fa576d8054b565f
elves trindade <elvestrindade31@gmail.com>
2023-06-14 17:50:34 +0100

    refactor: change post to get methods in cors test

--- src/main/middlewares/cors.test.ts
+++ src/main/middlewares/cors.test.ts
@@ -3,11 +3,11 @@ import app from '@main/config/app'
 
 describe('CORS Middleware', () => {
   it('Should anabled cors', async () => {
-    app.post('/test_cors', (req, res) => {
+    app.get('/test_cors', (req, res) => {
       res.send()
     })
     await request(app)
-      .post('/test_cors')
+      .get('/test_cors')
       .send()
       .expect('access-control-allow-origin', '*')
       .expect('access-control-allow-methods', '*')
