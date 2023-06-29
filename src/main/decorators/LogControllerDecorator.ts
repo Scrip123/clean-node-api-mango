4cb6328e96f9669c74fc04942b10c60e2a3b3759
elves trindade <elvestrindade31@gmail.com>
2023-06-29 09:16:42 +0100

    feat: ensure LogControllerDecorator returns the same result of the controller

--- src/main/decorators/LogControllerDecorator.ts
+++ src/main/decorators/LogControllerDecorator.ts
@@ -3,9 +3,7 @@ import { IController, IHttpRequest, IHttpResponse } from '@presentation/protocol
 export class LogControllerDecorator implements IController {
   constructor (private readonly controller: IController) {}
   async handle (httpRequest: IHttpRequest): Promise<IHttpResponse> {
-    const response = await this.controller.handle(httpRequest)
-    console.log(response)
-    return response
-    // return await new Promise(resolve => resolve(httpResonse))
+    const httpResponse = await this.controller.handle(httpRequest)
+    return httpResponse
   }
 }
