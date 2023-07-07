70239ce0c779d55348d02988bb8f478a682911a1
elves trindade <elvestrindade31@gmail.com>
2023-07-07 18:41:12 +0100

    feat: add LogErrorRepository to SignUpFactory

--- src/main/factories/signupFactory.ts
+++ src/main/factories/signupFactory.ts
@@ -1,6 +1,7 @@
 import { DbAddAcount } from '@data/useCases/addAccount/DbAddAccount'
 import { BcrypterAdapter } from '@infra/cryptografy/BcrypterAdapter'
 import { AccountMongoRepository } from '@infra/db/mongoDb/AccountRepository/AccountMongoRepository'
+import { LogErrorRepository } from '@infra/db/mongoDb/LogRepository/LogErrorRepository'
 import { LogControllerDecorator } from '@main/decorators/LogControllerDecorator'
 import { SignUpController } from '@presentation/controllers/signUp/SignUpController'
 import { IController } from '@presentation/protocols'
@@ -13,5 +14,6 @@ export const makeSignupController = (): IController => {
   const accountMongoRepository = new AccountMongoRepository()
   const dbAddAccount = new DbAddAcount(bcrypterAdapter, accountMongoRepository)
   const signUpController = new SignUpController(emailValidatorAdapter, dbAddAccount)
-  return new LogControllerDecorator(signUpController)
+  const logErrorRepository = new LogErrorRepository()
+  return new LogControllerDecorator(signUpController, logErrorRepository)
 }
