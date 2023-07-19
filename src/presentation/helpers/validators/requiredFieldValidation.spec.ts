7be95b35f63d928373f21082d7fd54b7cfaf36c1
elves trindade <elvestrindade31@gmail.com>
2023-07-19 16:13:42 +0100

    refactor: move sut creation to a factory method

--- src/presentation/helpers/validators/requiredFieldValidation.spec.ts
+++ src/presentation/helpers/validators/requiredFieldValidation.spec.ts
@@ -1,14 +1,17 @@
 import { MissingParamError } from '@presentation/errors'
 import { RequiredFieldValidation } from './RequiredFieldValidation'
 
+const makeSut = (): RequiredFieldValidation => {
+  return new RequiredFieldValidation('field')
+}
 describe('Required field name validation', () => {
   it('Should return a MissingParamError if validation fails', () => {
-    const sut = new RequiredFieldValidation('field')
+    const sut = makeSut()
     const error = sut.validate({ name: 'any_name' })
     expect(error).toEqual(new MissingParamError('field'))
   })
   it('Should not return Validation succeds', () => {
-    const sut = new RequiredFieldValidation('field')
+    const sut = makeSut()
     const error = sut.validate({ field: 'any_name' })
     expect(error).toBeFalsy()
   })
