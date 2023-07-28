64a90d5638fe200865f59094b1f2fff70814168d
elves trindade <elvestrindade31@gmail.com>
2023-07-28 23:18:24 +0100

    feat: ensure AutheticationUseCase calls HashComapre with correct values

--- src/data/useCases/authentication/Authentication.ts
+++ src/data/useCases/authentication/Authentication.ts
@@ -1,13 +1,18 @@
+import { IHashCompare } from '@data/protocols/cryptografy/IHashCompare'
 import { ILoadAccountByEmailRepository } from '@data/protocols/db/ILoadAccountByEmailRepository'
 import { IAuthentication, IAuthenticationModel } from '@domain/useCases/IAuthentication'
 
 export class AuthenticationUseCase implements IAuthentication {
   constructor (
-    private readonly loadAccountByEmailRepository: ILoadAccountByEmailRepository
+    private readonly loadAccountByEmailRepository: ILoadAccountByEmailRepository,
+    private readonly hashComapre: IHashCompare
   ) {}
 
   async auth (authentication: IAuthenticationModel): Promise<string> {
-    await this.loadAccountByEmailRepository.load(authentication.email)
+    const account = await this.loadAccountByEmailRepository.load(authentication.email)
+    if (account) {
+      await this.hashComapre.compare(authentication.password, account.password)
+    }
     return null
   }
 }
