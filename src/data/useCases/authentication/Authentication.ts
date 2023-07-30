ed54c5fd621c69027b5db139a6838e6caca60065
elves trindade <elvestrindade31@gmail.com>
2023-07-30 18:07:51 +0100

    feat: ensure AutheticationUseCase calls tokenGenerator  with correct id

--- src/data/useCases/authentication/Authentication.ts
+++ src/data/useCases/authentication/Authentication.ts
@@ -1,17 +1,20 @@
 import { IHashCompare } from '@data/protocols/cryptografy/IHashCompare'
 import { ILoadAccountByEmailRepository } from '@data/protocols/db/ILoadAccountByEmailRepository'
+import { ITokenGenerator } from '@data/protocols/db/ITokenGenerator'
 import { IAuthentication, IAuthenticationModel } from '@domain/useCases/IAuthentication'
 
 export class AuthenticationUseCase implements IAuthentication {
   constructor (
     private readonly loadAccountByEmailRepository: ILoadAccountByEmailRepository,
-    private readonly hashComapre: IHashCompare
+    private readonly hashComapre: IHashCompare,
+    private readonly tokenGenerator: ITokenGenerator
   ) {}
 
   async auth (authentication: IAuthenticationModel): Promise<string> {
     const account = await this.loadAccountByEmailRepository.load(authentication.email)
     if (account) {
       await this.hashComapre.compare(authentication.password, account.password)
+      await this.tokenGenerator.generate(account.id)
     }
     return null
   }
