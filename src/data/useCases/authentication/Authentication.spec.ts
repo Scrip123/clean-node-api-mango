fce970083cf8d9af7846a2f3b5fb0f204bd47b56
elves trindade <elvestrindade31@gmail.com>
2023-07-30 18:09:34 +0100

    test: ensure AutheticationUseCase calls tokenGenerator  with correct id

--- src/data/useCases/authentication/Authentication.spec.ts
+++ src/data/useCases/authentication/Authentication.spec.ts
@@ -3,9 +3,10 @@ import { IAccountModelDataBase } from '../addAccount/dbAddAccountProtocols'
 import { AuthenticationUseCase } from './Authentication'
 import { IAuthenticationModel } from '@domain/useCases/IAuthentication'
 import { IHashCompare } from '@data/protocols/cryptografy/IHashCompare'
+import { ITokenGenerator } from '@data/protocols/db/ITokenGenerator'
 
 const makeFakeAccount = (): IAccountModelDataBase => ({
-  id: 'id',
+  id: 'any_id',
   name: 'any_name',
   email: 'any_email@gmail.com',
   password: 'hashed_password'
@@ -27,19 +28,34 @@ const makeHahsComapre = (): IHashCompare => {
   }
   return new LoadAccountByEmailRepositoryStub()
 }
+const makeTokenGenerator = (): ITokenGenerator => {
+  class TokenGeneratorStub implements ITokenGenerator {
+    async generate (id: string): Promise<string> {
+      return await new Promise(resolve => resolve('any_token'))
+    }
+  }
+  return new TokenGeneratorStub()
+}
 interface ISutTypes {
   sut: AuthenticationUseCase
   loadAccountByEmailRepositoryStub: ILoadAccountByEmailRepository
   hashCompareStub: IHashCompare
+  tokenGeneratorStub: ITokenGenerator
 }
 const makeSut = (): ISutTypes => {
   const loadAccountByEmailRepositoryStub = makeLoadAccountByEmailRepository()
   const hashCompareStub = makeHahsComapre()
-  const sut = new AuthenticationUseCase(loadAccountByEmailRepositoryStub, hashCompareStub)
+  const tokenGeneratorStub = makeTokenGenerator()
+  const sut = new AuthenticationUseCase(
+    loadAccountByEmailRepositoryStub,
+    hashCompareStub,
+    tokenGeneratorStub
+  )
   return {
     sut,
     loadAccountByEmailRepositoryStub,
-    hashCompareStub
+    hashCompareStub,
+    tokenGeneratorStub
   }
 }
 const makeFakeAuthentication = (): IAuthenticationModel => ({
@@ -91,5 +107,11 @@ describe('DbAuthentication useCase', () => {
       .mockReturnValueOnce(new Promise(resolve => resolve(false)))
     const accessToken = await sut.auth(makeFakeAuthentication())
     expect(accessToken).toBeNull()
+  })
+  it('Should call toknGenerator with correct id', async () => {
+    const { sut, tokenGeneratorStub } = makeSut()
+    const tokenSpy = jest.spyOn(tokenGeneratorStub, 'generate')
+    await sut.auth(makeFakeAuthentication())
+    expect(tokenSpy).toHaveBeenLastCalledWith('any_id')
   })
 })
