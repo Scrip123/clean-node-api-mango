c465bed1872f71e8d8372f0756fd0616e37525a8
elves trindade <elvestrindade31@gmail.com>
2023-08-04 16:37:02 +0100

    refactor: rename method and remove git add in lintstagefile

--- src/data/useCases/authentication/Authentication.ts
+++ src/data/useCases/authentication/Authentication.ts
@@ -11,7 +11,7 @@ export class AuthenticationUseCase implements IAuthentication {
   constructor (
     private readonly loadAccountByEmailRepository: ILoadAccountByEmailRepository,
     private readonly hashComapre: IHashCompare,
-    private readonly tokenGenerator: IEncrypterToken,
+    private readonly encrypterToken: IEncrypterToken,
     private readonly updateAccessTokenRepository: IUpdateAccessTokenRepository
   ) {}
 
@@ -20,7 +20,7 @@ export class AuthenticationUseCase implements IAuthentication {
     if (account) {
       const isValid = await this.hashComapre.compare(authentication.password, account.password)
       if (isValid) {
-        const accessToken = await this.tokenGenerator.encrypt(account.id)
+        const accessToken = await this.encrypterToken.encrypt(account.id)
         await this.updateAccessTokenRepository.update(account.id, accessToken)
         return accessToken
       }
