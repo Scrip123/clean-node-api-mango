7bdd5b8eaea84d958bcf2a0a9563c3dba1d545ea
elves trindade <elvestrindade31@gmail.com>
2023-08-08 11:38:38 +0100

    feat: ensure JwtAdapter returns a token when sign succeds

--- src/infra/cryptografy/jwtAdapter/JwtAdapter.ts
+++ src/infra/cryptografy/jwtAdapter/JwtAdapter.ts
@@ -4,7 +4,7 @@ import { IHasher } from '@data/protocols/cryptografy/IHasher'
 export class JwtAdapter implements IHasher {
   constructor (private readonly secret: string) {}
   async hash (value: string): Promise<string> {
-    jwt.sign({ id: value }, this.secret)
-    return null
+    const accessToken = jwt.sign({ id: value }, this.secret)
+    return accessToken
   }
 }
