a1375cf44a90cd2f3dd615798759a67635e5b759
elves trindade <elvestrindade31@gmail.com>
2023-08-08 11:44:44 +0100

    test: ensure JwtAdapter returns a token when sign succeds

--- src/infra/cryptografy/jwtAdapter/JwtAdapter.spec.ts
+++ src/infra/cryptografy/jwtAdapter/JwtAdapter.spec.ts
@@ -1,15 +1,20 @@
 import jwt from 'jsonwebtoken'
 import { JwtAdapter } from './JwtAdapter'
-/* jest.mock('jwt', () => ({
+jest.mock('jsonwebtoken', () => ({
   async sign (): Promise<string> {
     return await new Promise(resolve => resolve('any_token'))
   }
-})) */
+}))
 describe('Jwt Adapter', () => {
   it('Should call sign with correct values', async () => {
     const sut = new JwtAdapter('secrect')
     const signSpy = jest.spyOn(jwt, 'sign')
     await sut.hash('any_id')
     expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secrect')
+  })
+  it('Should a token sign on sign success', async () => {
+    const sut = new JwtAdapter('secrect')
+    const accessToken = await sut.hash('any_id')
+    expect(accessToken).toBe('any_token')
   })
 })
