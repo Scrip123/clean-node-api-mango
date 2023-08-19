075e9b4a1591937a045cc1df9dad0992b1e57fc5
elves trindade <elvestrindade31@gmail.com>
2023-08-19 09:10:12 +0100

    chore: change dev script to use docker compose

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,3 @@ FROM node:20
 WORKDIR /usr/clean_node_api_mango
 COPY package.json ./
 RUN npm install
-COPY dist ./dist
-EXPOSE 3000
-CMD ["npm", "start"]
--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  MONGO_URL: process.env.MONGODB_URL || 'mongodb://localhost:27017/clean-node-api-mango',
+  MONGO_URL: process.env.MONGODB_URL || 'mongodb://mongo:27017/clean_node_api_mango',
   PORT: process.env.PORT || 3000,
   JWT_SECRET: process.env.JWT_SECRET || 'danger=codeds'
 }
