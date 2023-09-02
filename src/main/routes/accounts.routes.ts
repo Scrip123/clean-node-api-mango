71e1543872a7b96862ae164f01a30fc92bf2cf5b
elves trindade <elvestrindade31@gmail.com>
2023-09-02 14:55:21 +0100

    refactor: refactor main/factories/controllers folders

--- src/main/routes/accounts.routes.ts
+++ src/main/routes/accounts.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express'
 import { adapteRoute } from '@main/adapter/express/expressRoutesAdapter'
-import { makeLoginController } from '@main/factories/controllers/login/loginControllerFactory'
-import { makeSignupController } from '@main/factories/controllers/signUp/signupControllerFactory'
+import { makeLoginController } from '@main/factories/controllers/controllersAccountsFactories/login/loginControllerFactory'
+import { makeSignupController } from '@main/factories/controllers/controllersAccountsFactories/signUp/signupControllerFactory'
 
 export default (router: Router): void => {
   router.post('/signup', adapteRoute(makeSignupController()))
