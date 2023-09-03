7a20c85f35b11222049ba5f47ebb3b2e037e719e
elves trindade <elvestrindade31@gmail.com>
2023-09-03 23:22:51 +0100

    feat: ensure AddSurveyController return badRequest 400 if  Validation fails

--- src/presentation/controllers/surveys/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/surveys/add-survey/add-survey-controller.ts
@@ -1,9 +1,11 @@
+import { badRequest } from '@presentation/helpers/http/httpHelper'
 import { IController, IHttpRequest, IHttpResponse, IValidation } from './add-survey-protocols'
 
 export class AddSurveyController implements IController {
   constructor (private readonly validation: IValidation) {}
   async handle (httpRequest: IHttpRequest): Promise<IHttpResponse> {
-    this.validation.validate(httpRequest.body)
+    const error = this.validation.validate(httpRequest.body)
+    if (error) return badRequest(error)
     return await new Promise(resolve => resolve(null))
   }
 }
