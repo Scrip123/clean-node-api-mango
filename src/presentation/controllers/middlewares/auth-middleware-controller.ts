7627797936cf25e4c472e85349d50b8a0c7795d8
elves trindade <elvestrindade31@gmail.com>
2023-09-14 10:57:22 +0100

    feat: ensure AuthMiddlewareController return 500 if LoadAccountByToken throws

--- src/presentation/controllers/middlewares/auth-middleware-controller.ts
+++ src/presentation/controllers/middlewares/auth-middleware-controller.ts
@@ -1,15 +1,19 @@
 import { AccessDeniedError } from '@presentation/errors/Access-denied-error'
-import { forBidden, ok } from '@presentation/helpers/http/httpHelper'
+import { forBidden, ok, serverError } from '@presentation/helpers/http/httpHelper'
 import { IHttpRequest, IHttpResponse, ILoadAccountByToken, IMiddleware } from './auth-middleware-controller-protocols'
 
 export class AuthMiddlewareController implements IMiddleware {
   constructor (private readonly loadAccountByToken?: ILoadAccountByToken) {}
   async handle (httpRequest: IHttpRequest): Promise<IHttpResponse> {
-    const accessToken = httpRequest.headers?.['x-access-token']
-    if (accessToken) {
-      const account = await this.loadAccountByToken.load(accessToken)
-      if (account) return ok({ accountId: account.id })
+    try {
+      const accessToken = httpRequest.headers?.['x-access-token']
+      if (accessToken) {
+        const account = await this.loadAccountByToken.load(accessToken)
+        if (account) return ok({ accountId: account.id })
+      }
+      return forBidden(new AccessDeniedError())
+    } catch (error) {
+      return serverError(error)
     }
-    return forBidden(new AccessDeniedError())
   }
 }
