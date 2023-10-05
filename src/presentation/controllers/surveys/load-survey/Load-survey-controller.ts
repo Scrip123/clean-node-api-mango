0149c70fa8a12514a007217c780383e411115c9f
elves trindade <elvestrindade31@gmail.com>
2023-10-05 17:58:59 +0100

    feat: ensure LoadSurveyController returns 200 on success

--- src/presentation/controllers/surveys/load-survey/Load-survey-controller.ts
+++ src/presentation/controllers/surveys/load-survey/Load-survey-controller.ts
@@ -1,8 +1,9 @@
+import { ok } from '@presentation/helpers/http/httpHelper'
 import { IController, IHttpRequest, IHttpResponse, ILoadSurveyUseCaseDomain } from './load-survey-controller-protocols'
 export class LoadSurveyController implements IController {
   constructor (private readonly loadSurveysUseCase: ILoadSurveyUseCaseDomain) {}
   async handle (httRequest: IHttpRequest): Promise<IHttpResponse> {
-    await this.loadSurveysUseCase.loadSurveys()
-    return null
+    const surveys = await this.loadSurveysUseCase.loadSurveys()
+    return ok(surveys)
   }
 }
