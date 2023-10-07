f673a0969222fc2ec4b290827a115a70a2e76396
elves trindade <elvestrindade31@gmail.com>
2023-10-07 19:00:57 +0100

    test: ensure SurveyMongoRepository load all surveys

--- src/infra/db/mongoDb/surveys-repositories/surveys-mongo-repository.spec.ts
+++ src/infra/db/mongoDb/surveys-repositories/surveys-mongo-repository.spec.ts
@@ -17,19 +17,53 @@ describe('Survey MongoDb Repository', () => {
     surveysCollection = await MongoHelper.getCollection('surveys')
     surveysCollection.deleteMany({})
   })
-  it('Should add a survey on success', async () => {
-    const sut = makeSut()
-    await sut.add({
-      question: 'any_question',
-      answers: [{
-        image: 'any_image',
-        answer: 'any_answer'
-      }, {
-        answer: 'other_answer'
-      }],
-      createdAt: new Date()
+
+  describe('add surveys()', () => {
+    it('Should add a survey on success', async () => {
+      const sut = makeSut()
+      await sut.add({
+        question: 'any_question',
+        answers: [{
+          image: 'any_image',
+          answer: 'any_answer'
+        }, {
+          answer: 'other_answer'
+        }],
+        createdAt: new Date()
+      })
+      const survey = await surveysCollection.findOne({ question: 'any_question' })
+      expect(survey).toBeTruthy()
     })
-    const survey = await surveysCollection.findOne({ question: 'any_question' })
-    expect(survey).toBeTruthy()
+  })
+  describe('loadAll surveys()', () => {
+    it('Should load all surveys on success', async () => {
+      await surveysCollection.insertMany([
+        {
+          question: 'any_question',
+          answers: [{
+            image: 'any_image',
+            answer: 'any_answer'
+          }, {
+            answer: 'other_answer'
+          }],
+          createdAt: new Date()
+        },
+        {
+          question: 'any_question2',
+          answers: [{
+            image: 'any_image2',
+            answer: 'any_answer2'
+          }, {
+            answer: 'other_answer2'
+          }],
+          createdAt: new Date()
+        }
+      ])
+      const sut = makeSut()
+      const surveys = await sut.loadAllSurveys()
+      expect(surveys.length).toBe(2)
+      expect(surveys[0].question).toBe('any_question')
+      expect(surveys[1].question).toBe('any_question2')
+    })
   })
 })
