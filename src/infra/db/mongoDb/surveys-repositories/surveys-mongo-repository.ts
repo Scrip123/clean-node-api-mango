21cf34fff293e59f4e0ea84187252ac889deef57
elves trindade <elvestrindade31@gmail.com>
2023-10-07 18:57:49 +0100

    feat: ensure SurveyMongoRepository load all surveys

--- src/infra/db/mongoDb/surveys-repositories/surveys-mongo-repository.ts
+++ src/infra/db/mongoDb/surveys-repositories/surveys-mongo-repository.ts
@@ -1,10 +1,17 @@
 import { IAddSurveyRepository } from '@data/protocols/db/surveys/IAdd-survey-repository'
 import { MongoHelper } from '../helpers/mongoHelper'
-import { ISurveyInputModelDTO } from '@domain/models/ISurvey-model-domain'
+import { ISurveyInputModelDTO, ISurveyOutputModelDTO } from '@domain/models/ISurvey-model-domain'
+import { ILoadSurveysRepository } from '@data/protocols/db/surveys/ILoad-survey-repository'
 
-export class SurveyMongoRepository implements IAddSurveyRepository {
+export class SurveyMongoRepository implements IAddSurveyRepository, ILoadSurveysRepository {
   async add (data: ISurveyInputModelDTO): Promise<void> {
     const surveysCollecction = await MongoHelper.getCollection('surveys')
     await surveysCollecction.insertOne(data)
+  }
+
+  async loadAllSurveys (): Promise<ISurveyOutputModelDTO[]> {
+    const surveysCollecction = await MongoHelper.getCollection('surveys')
+    const surveys: ISurveyOutputModelDTO[] = await surveysCollecction.find().toArray()
+    return surveys
   }
 }
