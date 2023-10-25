86574d5060a14e8f3375c488a9fecf63683b2371
elves trindade <elvestrindade31@gmail.com>
2023-10-25 20:14:21 +0100

    feat: ensure SaveSurveyResultController return 500 if loadSurveyByIdStub throws

--- src/presentation/controllers/survey-results/save-survey-result/Save-survey-result-controller.ts
+++ src/presentation/controllers/survey-results/save-survey-result/Save-survey-result-controller.ts
@@ -1,13 +1,17 @@
 import { ILoadSurveyByIdDomain } from '@domain/useCases/surveys-domain-usecases/ILoad-survey-by-id'
 import { InvalidParamError } from '@presentation/errors'
-import { forBidden } from '@presentation/helpers/http/httpHelper'
+import { forBidden, serverError } from '@presentation/helpers/http/httpHelper'
 import { IController, TypesHttpRequest, TypesHttpResponse } from '@presentation/protocols'
 
 export class SaveSurveyResultController implements IController {
   constructor (private readonly loadSurveyByIdUseCase: ILoadSurveyByIdDomain) {}
   async handle (httpRequest: TypesHttpRequest): Promise<TypesHttpResponse> {
-    const survey = await this.loadSurveyByIdUseCase.loadSurveyById(httpRequest.params.surveyId)
-    if (!survey) return forBidden(new InvalidParamError('surveyId'))
-    return null
+    try {
+      const survey = await this.loadSurveyByIdUseCase.loadSurveyById(httpRequest.params.surveyId)
+      if (!survey) return forBidden(new InvalidParamError('surveyId'))
+      return null
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
