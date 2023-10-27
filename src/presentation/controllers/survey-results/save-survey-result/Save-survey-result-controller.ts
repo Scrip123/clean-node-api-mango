bc7fb4a0fc0f5a77ee93d0db3cba8cb83818f521
elves trindade <elvestrindade31@gmail.com>
2023-10-27 12:46:00 +0100

    feat: ensure SaveSurveyResultController calls saveSurveyResultUseCaseStub with correct values

--- src/presentation/controllers/survey-results/save-survey-result/Save-survey-result-controller.ts
+++ src/presentation/controllers/survey-results/save-survey-result/Save-survey-result-controller.ts
@@ -1,14 +1,20 @@
 import { ILoadSurveyByIdDomain } from '@domain/useCases/surveys-domain-usecases/ILoad-survey-by-id'
+import { ISaveSurveyResultDomain } from '@domain/useCases/surveys-result-domain-usecase/ISave-survey-result-model'
 import { InvalidParamError } from '@presentation/errors'
 import { forBidden, serverError } from '@presentation/helpers/http/httpHelper'
 import { IController, TypesHttpRequest, TypesHttpResponse } from '@presentation/protocols'
 
 export class SaveSurveyResultController implements IController {
-  constructor (private readonly loadSurveyByIdUseCase: ILoadSurveyByIdDomain) {}
+  constructor (
+    private readonly loadSurveyByIdUseCase: ILoadSurveyByIdDomain,
+    private readonly saveSurveyResultUseCase: ISaveSurveyResultDomain
+  ) {}
+
   async handle (httpRequest: TypesHttpRequest): Promise<TypesHttpResponse> {
     try {
       const { surveyId } = httpRequest.params
       const { answer } = httpRequest.body
+      const { accountId } = httpRequest
       const survey = await this.loadSurveyByIdUseCase.loadSurveyById(surveyId)
       if (!survey) return forBidden(new InvalidParamError('surveyId'))
 
@@ -16,6 +22,13 @@ export class SaveSurveyResultController implements IController {
       if (!answers.includes(answer)) {
         return forBidden(new InvalidParamError('answer'))
       }
+
+      await this.saveSurveyResultUseCase.save({
+        surveyId,
+        accountId,
+        answer,
+        createdAt: new Date()
+      })
       return null
     } catch (error) {
       return serverError(error)
